9c2c6b153fc2f6dbe0b30c0bacdad3c8b9b03d62
Mads Liu <lxzmads@gmail.com>
2019-07-20 18:38:35 +0800

    * Update color tab.

--- zsh/zlogin.symlink.sh
+++ zsh/zlogin.symlink.sh
@@ -5,7 +5,7 @@ if [ -n "$IT2_SESSION_COLOR" ]; then
     # clear
 else
     # Change iterm2's tab color automatically
-    hostmd5=$(hostname | md5 || md5sum );
+    hostmd5=$(hostname | (md5 2>/dev/null || md5sum 2>/dev/null ));
     colorseed=$((16#${hostmd5:0:4}));
     colorrgb=$(python3 $DOTFILES/zsh/colorutil.py $colorseed)
     it2-tab-color $colorrgb
