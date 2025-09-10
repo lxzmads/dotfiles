1203b8d0998b687f9027ea6569050dfbe99a566e
admin <admin@punch.local>
2025-09-10 22:14:00 +0800

    update

--- docker/functions.zsh
+++ docker/functions.zsh
@@ -6,9 +6,9 @@ de(){
 }
 function dsm(){ docker stop $1 && docker rm $1 || echo "failed!!!";}
 function mc(){ ssh root@$1 }
-function md_con() { docker run -it --rm -v `pwd`:/data -w /data $1 }
-function md_ubuntu() { docker run -it --rm -v `pwd`:/data -w /data ubuntu:$1 }
-function md_neo() {
+function vm() { docker run -it --rm -v `pwd`:/data -w /data $1 }
+function vm_ubuntu() { docker run -it --rm -v `pwd`:/data -w /data ubuntu:$1 }
+function vm_neo() {
     docker ps -a -f 'name=ubuntu_neo' | grep ubuntu_neo >/dev/null
     if (( $? == 0 ));then
         docker start ubuntu_neo >/dev/null && docker exec -it ubuntu_neo /bin/zsh
@@ -16,13 +16,13 @@ function md_neo() {
         docker run -it -v `pwd`:/data -w /data --name ubuntu_neo --entrypoint /bin/zsh ubuntu:neo
     fi
 }
-function md_stego() { docker run -it --rm -p 127.0.0.1:6901:6901 -v `pwd`:/data dominicbreuker/stego-toolkit bash }
-function md_pwn(){
+function vm_stego() { docker run -it --rm -p 127.0.0.1:6901:6901 -v `pwd`:/data dominicbreuker/stego-toolkit bash }
+function vm_pwn(){
     docker run -it --rm -v `pwd`:/ctf/work -p23946:23946 --cap-add=SYS_PTRACE lxzmads/pwndbgenv:14.04
 }
-function md_php(){
+function vm_php(){
     if (( $# != 2 )); then
-        echo "Usage:  md php <port> <version>";
+        echo "Usage:  vm php <port> <version>";
     else
         docker run -d --name php-$2 -v `pwd`:/var/www/html -p $1:80 php:$2-apache;
         if (( $? == 0 ));then
@@ -31,13 +31,13 @@ function md_php(){
     fi
 }
 
-function md_php_nginx(){
+function vm_php_nginx(){
     echo "UNIMPLEMENT"
 }
 
-function md_tomcat(){
+function vm_tomcat(){
     if (( $# != 2 )); then
-        echo "Usage:  md tomcat <port> <version>";
+        echo "Usage:  vm tomcat <port> <version>";
     else
         docker run -dit --name tomcat-$2 -v `pwd`:/usr/local/tomcat/webapps -p $1:8080 tomcat:$2;
         if (( $? == 0 ));then
@@ -46,8 +46,8 @@ function md_tomcat(){
     fi
 }
 
-function md:usage(){
-    echo "Usage:  md COMMAND
+function vm:usage(){
+    echo "Usage:  vm COMMAND
 
     Make a BRAND NEW CLEAN SELF DESTRUCTION DOCKER environment (not f**k in CN), whatever :)
 
@@ -61,27 +61,27 @@ function md:usage(){
     ";
 }
 
-function md(){
+function vm(){
     if (( $# < 1 )); then
-        md:usage;
+        vm:usage;
     else
         case $1 in
             php)
-                md_php $2 $3;;
+                vm_php $2 $3;;
             tomcat)
-                md_tomcat $2 $3;;
+                vm_tomcat $2 $3;;
             pwn)
-                md_pwn;;
+                vm_pwn;;
             stego)
-                md_stego;;
+                vm_stego;;
             ubuntu)
-                md_ubuntu $2;;
+                vm_ubuntu $2;;
             neo)
-                md_neo $2;;
+                vm_neo $2;;
             con)
-                md_con $2;;
+                vm_con $2;;
             *)
-                md:usage;;
+                vm:usage;;
         esac
     fi
 }
